9fb915fac61722117e85884eab8a864bb92280c1
Nik Hixson <nikilobe@gmail.com>
2024-09-26 16:04:27 -0700

    completed the project

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🤢🤢🤢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🚫🚫🚫 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
