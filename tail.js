db650656bb45c7fc015d9969dabe0b6fb712da5b
Nik Hixson <nikilobe@gmail.com>
2024-09-27 12:41:44 -0700

    ok now I completed the tail project

--- tail.js
+++ tail.js
@@ -17,3 +17,6 @@ const tail = function(array) {
 
 const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
