adc9f5f008c9d0937ea8b9879cd6cae73275661c
Nik Hixson <nikilobe@gmail.com>
2024-09-29 14:02:30 -0700

    completed flatten.js

---
+++ flatten.js
@@ -0,0 +1,38 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+function assertArraysEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🤢 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+
+//Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
+
+function flatten(array) {
+  console.log(array.flat());
+  // for(let i = 0; i < array.length; i++) {
+    // if (array[i])
+  // }
+}
+
+
+
+
+flatten([1, 2, ['combo'], 'of', ['string']]);
+flatten([1, 2, [3, 4], 5, [6]]);
+flatten([1, 2, [4], 5, []]);
+flatten([[3, 4],[6]]);
