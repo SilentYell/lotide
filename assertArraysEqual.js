4986af0beb56ea3228124b502cbadaf2150d54dd
Nik Hixson <nikilobe@gmail.com>
2024-09-29 12:48:37 -0700

    adjusted code according to feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,10 @@
 function eqArrays(arr1, arr2) {
-  return arr1.toString() === arr2.toString();
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function assertArraysEqual(arr1, arr2) {
