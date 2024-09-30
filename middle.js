4965ad9c38b2d2235251fd4af061958230282e1a
Nik Hixson <nikilobe@gmail.com>
2024-09-29 20:30:57 -0700

    clarified requirements in results

--- middle.js
+++ middle.js
@@ -34,9 +34,9 @@ function middle(array) {
   return arrMid;
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), [1]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 6]);
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
