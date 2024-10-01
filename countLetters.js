caff70588604fe99b05be74e6cd0d865578dd339
Nik Hixson <nikilobe@gmail.com>
2024-10-01 12:09:58 -0700

    learned some better ways to loop and impemented it here. I also added toLowerCase to account for upper case letters

--- countLetters.js
+++ countLetters.js
@@ -8,42 +8,22 @@ function assertEqual(actual, expected) {
 
 
 function countLetters(input) {
-  
   let result = {};
+  input = input.toLowerCase();
 
-  for (letters of input) {
-    //replace the spaces with nothing if the string has spaces
-    letters.replace(' ', '');
-    //split the string into individual letters into a new array
-    letters.split('');
-    //looping through the letters array
-    for (let i = 1; i < letters.length; i++) {
-      //creating variable for key letter and value number to be added to result obj
-      //test letter should start as the first letter of letters array
-      let testLetter = letters[0];
-      //count should start at 1 since the first letter will be stored already
-      let count = 1;
-      //
-      if (testLetter !== letters[i]) {
-        //testLetter should be changed to the letters[i] value
-        testLetter = letters[i];
-        //count should reset to 1
-        count = 1;
-        //add the letter and count number to the result obj
-        result.testLetter = count;
+  for (letter of input) {
+    if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter]++;
       } else {
-        //count should increase
-        count ++;
-          //add the letter and count number to the result obj
-          result.testLetter = count;
+        result[letter] = 1;
       }
     }
-    //return the result object
-    return result;
   }
+  return result;
 }
 
 
-console.log(countLetters('icing'));//{i: 2, c: 1, n: 1, g: 1}
+console.log(countLetters('icing'));
 console.log(countLetters('Icing'));
-console.log(countLetters('nik is a cool guy i think'));
+console.log(countLetters('nik Is cool'));
