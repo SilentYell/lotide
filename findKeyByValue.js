3a3e5eb2b8b060ed29eb2f00168612f76f7dc42a
Nik Hixson <nikilobe@gmail.com>
2024-10-02 10:21:15 -0700

    fixed eslint errors

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🤢🤢🤢 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -9,14 +9,10 @@ const assertEqual = function (actual, expected) {
 
 
 const findKeyByValue = function(showObj, value) {
-  //convert input into lower case
   const lowerCase = value.toLowerCase();
 
-  //scan object for a key with the same value as value
   for (let key in showObj) {
-    //comparing the lower case version of the object key value to the lower case function value
     if (showObj[key].toLowerCase() === lowerCase) {
-      //return the original correct object key value
       return key;
     }
   }
@@ -24,7 +20,7 @@ const findKeyByValue = function(showObj, value) {
 
 
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
@@ -34,5 +30,5 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "the expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "the EXPANSe"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "the expanse"), "sciFi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "the EXPANSe"), "sciFi");
