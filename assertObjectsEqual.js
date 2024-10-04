57d5b559ff13e5604ed9170a4f00edb3697045d7
Nik Hixson <nikilobe@gmail.com>
2024-10-04 08:50:56 -0700

    finished the assignment following directions

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -32,11 +32,13 @@ const eqObjects = function(object1, object2) {
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
-  
+
   if (eqObjects(actual, expected)) {
-    console.log(`🤢 Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `🤢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `🚫 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
