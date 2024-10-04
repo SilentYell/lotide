497380a6bd08932f1b5ca8f400295df4d555ed03
Nik Hixson <nikilobe@gmail.com>
2024-10-04 08:32:40 -0700

    following along

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,13 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
@@ -20,5 +30,19 @@ const eqObjects = function(object1, object2) {
 };
 
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  
+  if (eqObjects(actual, expected)) {
+    console.log(`🤢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
+const obj1 = {first: 1, second: 2, third: 3};
+
+
+assertObjectsEqual(obj1, {first: 1, second: 2, third: 3});
+assertObjectsEqual(obj1, {first: 1, second: 22, third: 3});
 
