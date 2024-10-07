8922786436fb2bd0debd07b1d779093370b87c0f
Nik Hixson <nikilobe@gmail.com>
2024-10-07 09:08:57 -0700

    fixed eqArrays due to AI feedback for arr1.length !== arr2.length check

--- takeUntil.js
+++ takeUntil.js
@@ -26,6 +26,9 @@ console.log(results2);
 
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
