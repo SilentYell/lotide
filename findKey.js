11bfea849e873564f5a01188d4be0c04228946fa
Nik Hixson <nikilobe@gmail.com>
2024-10-07 12:56:28 -0700

    added test cases

--- findKey.js
+++ findKey.js
@@ -16,8 +16,7 @@ const findKey = function(object, callback) {
   }
 };
 
-
-findKey(
+assertEqual(findKey(
   {
     "Blue Hill": { stars: 1 },
     Akaleri: { stars: 3 },
@@ -27,5 +26,40 @@ findKey(
     Akelarre: { stars: 3 },
   },
   (x) => x.stars === 2
-); // => "noma"
+), 'noma');
 
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 5
+), undefined);
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 1
+), 'Blue Hill');
+
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 3
+), 'Akaleri');
