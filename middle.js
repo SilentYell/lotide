f545bad223b89e7281f29747056e3d6322be09ce
Nik Hixson <nikilobe@gmail.com>
2024-10-17 14:45:02 -0700

    Refactored the middle.js file and moved the test code to a new middleTest.js file

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🤢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🚫 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 const middle = function(array) {
   let arrMid = [];
   if (array.length < 3) {
@@ -33,9 +12,4 @@ const middle = function(array) {
   return arrMid;
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
