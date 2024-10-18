aa695971ee54b1d8508a9c7b12bcec3ef4cccb94
Nik Hixson <nikilobe@gmail.com>
2024-10-17 21:31:14 -0700

    changed some test cases

--- tail.js
+++ tail.js
@@ -1,5 +1,5 @@
 const tail = function(array) {
-  return array.slice(1).toString();
+  return array.slice(1);
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,17 @@
 const assert = require('chai').assert;
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 describe("#tail", () => {
   
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"].toString());
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
   });
 
-  it("returns ['Lighthouse'] for [' Lighthouse']", () => {
-    assert.deepEqual(tail([" Lighthouse"].toString()), ["Lighthouse"].toString());
-  });
-
-  it("returns [] for []", () => {
-    assert.deepEqual(tail([].toString()), [].toString());
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(["Hello"]), []);
   });
   
-  it("returns [6] for [' 6']", () => {
-    assert.deepEqual(tail([" 6"].toString()), [6].toString());
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
   });
 });
